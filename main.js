ceb3afe3da3460294ae21c0e2aa3e9fe0cb0abfd
Matheus Cristofoli <matfrietasc@gmail.com>
2023-09-07 17:12:22 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const dialog = document.getElementById('dialog')
+const unlockBtn = document.getElementById('unlockButton')
+const closeBtn = document.getElementById('closebtn')
+
+closeBtn.addEventListener('click', () => {
+	dialog.close()
+})
+
+function openVault() {
+	// Extra code for fun!
+	// this checks if the
+	// if (document.getElementById('unlockButton').innerHTML === 'unlocked') {
+	// 	document.getElementById('unlockButton').innerHTML = 'unlock'
+	// 	return
+	// }
+
+	// 1. Appends the message to the Modal Box
+	document.getElementById('message').innerText =
+		'You have received this message because you have been chosen to open an important vault.\n\nHere is the secret combination: '
+
+	//2. Using addition to generate the first code (10)
+	let firstCode = 5 + 5
+	//3.  Using multiplication to generate the second code (40)
+	let secondCode = 10 * 4
+	//4. Using subtraction to generate the third code (39)
+	let thirdCode = 50 - 11
+	//5. Combine the three codes into a single string inside a span
+	let combination = `<span class="red-text">${firstCode} - ${secondCode} - ${thirdCode}</span>`
+
+	//Display the codes
+	//6. Using a dialog box to display the vault codes
+	document.getElementById('cbntn').innerHTML = combination
+
+	//7. Opens the Dialog Box
+	dialog.showModal()
+}
